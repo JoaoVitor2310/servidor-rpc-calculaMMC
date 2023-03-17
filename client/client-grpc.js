b180fe911cc21dedfba99d68027896931a86b596
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-03-17 20:26:46 -0300

    quase finalizada, falta só conseguir passar os argumentos para a função

--- client/client-grpc.js
+++ client/client-grpc.js
@@ -1,8 +1,24 @@
-const grpc = require('grpc'); 
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
 const PROTO_PATH = '../notes.proto'; // Estamos passando o endereço do arquivo .proto
-const MMC = grpc.load(PROTO_PATH).MMC
 
-const client = new MMC('localhost:30031', // Criando uma chamada com o server
-    grpc.credentials.createInsecure());
-    
-module.exports = client;
+var packageDefinition = protoLoader.loadSync(PROTO_PATH);
+var notes_proto = grpc.loadPackageDefinition(packageDefinition).notes;
+
+// const MMC = grpc.load(PROTO_PATH).MMC
+
+const client = new notes_proto.MMC('localhost:30031', grpc.credentials.createInsecure()); // Criando uma chamada com o server
+
+let args = process.argv.slice(2);
+// console.log(args[0], args[1]);
+let argumentos = {primeiro: Number(args[0]), segundo: Number(args[1])}
+
+// const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
+
+client.calculandoMMC(argumentos, (error, response) => { // Chamando o metodo List RPC e retornando os dados no console
+    if (error) {
+        console.log(error);
+    } else {
+        console.error(response.resposta);
+    }
+})
--- client/get_notes.js
+++ client/get_notes.js
@@ -1,9 +1,13 @@
-const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
+// let args = process.argv.slice(2);
+// // console.log(args[0], args[1]);
+// let argumentos = {primeiro: Number(args[0]), segundo: Number(args[1])}
 
-client.numeros({}, (error, variaveis) => { // Chamando o metodo List RPC e retornando os dados no console
-    if (!error) {
-        console.log(variaveis);
-    } else {
-        console.error(error);
-    }
-})
+// const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
+
+// client.resposta(argumentos, (error, response) => { // Chamando o metodo List RPC e retornando os dados no console
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.error(response.resposta);
+//     }
+// })
