e379da85966db43e9e4d35029259cc2c74f6d840
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-03-18 16:35:20 -0300

    client e server funcionando, falta somente a resposta do mmc voltar, ainda n descobri pq isso acontece

--- client/client-grpc.js
+++ client/client-grpc.js
@@ -7,18 +7,26 @@ var notes_proto = grpc.loadPackageDefinition(packageDefinition).notes;
 
 // const MMC = grpc.load(PROTO_PATH).MMC
 
-const client = new notes_proto.MMC('localhost:30031', grpc.credentials.createInsecure()); // Criando uma chamada com o server
+const main = () => {
+    console.log('main');
+    const client = new notes_proto.MMC('localhost:30031', grpc.credentials.createInsecure()); // Criando uma chamada com o server
 
-let args = process.argv.slice(2);
-// console.log(args[0], args[1]);
-let argumentos = {primeiro: Number(args[0]), segundo: Number(args[1])}
+    let args = process.argv.slice(2);
+    // console.log(args[0], args[1]);
+    let argumentos = { primeiro: Number(args[0]), segundo: Number(args[1]) }
+    console.log(argumentos);
+    // const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
 
-// const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
+    client.calculandoMMC(argumentos, (error, response) => { // Chamando o metodo List RPC e retornando os dados no console
+        console.log('client calculando mmc');
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(response.resultado);
+        }
+    })
+}
 
-client.calculandoMMC(argumentos, (error, response) => { // Chamando o metodo List RPC e retornando os dados no console
-    if (error) {
-        console.log(error);
-    } else {
-        console.error(response.resposta);
-    }
-})
+main();
+
+
