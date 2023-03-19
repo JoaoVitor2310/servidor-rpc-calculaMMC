8cefef3a2e3981ed8815d79e591b5d32bc824fb9
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2023-03-19 20:29:26 -0300

    servidor RPC concluído, retornando o MMC e com o código comentado

--- client/client-grpc.js
+++ client/client-grpc.js
@@ -1,32 +1,23 @@
-var grpc = require('@grpc/grpc-js');
-var protoLoader = require('@grpc/proto-loader');
-const PROTO_PATH = '../notes.proto'; // Estamos passando o endereço do arquivo .proto
+var grpc = require('@grpc/grpc-js'); // Chamada do pacote ‘grpc’
+var protoLoader = require('@grpc/proto-loader'); // Chamada do pacote proto-loader’, que irá ler o arquivo .proto
+const PROTO_PATH = '../model.proto'; // Guarda o endereço do arquivo .proto
 
-var packageDefinition = protoLoader.loadSync(PROTO_PATH);
-var notes_proto = grpc.loadPackageDefinition(packageDefinition).notes;
+var packageDefinition = protoLoader.loadSync(PROTO_PATH); // Lê o arquivo .proto
+var model_proto = grpc.loadPackageDefinition(packageDefinition).model; // Lê o arquivo .proto
 
-// const MMC = grpc.load(PROTO_PATH).MMC
+const main = () => { // Função main para executar o client
+    const client = new model_proto.MMC('localhost:30031', grpc.credentials.createInsecure()); // Cria uma chamada com o servidor
+    
+    let args = process.argv.slice(2); // Args irá identificar os números digitados pelo usuário depois do comando "node client-grpc.js"
+    let argumentos = { primeiro: Number(args[0]), segundo: Number(args[1]) } // Objeto "argumentos", com os números do client
 
-const main = () => {
-    console.log('main');
-    const client = new notes_proto.MMC('localhost:30031', grpc.credentials.createInsecure()); // Criando uma chamada com o server
-
-    let args = process.argv.slice(2);
-    // console.log(args[0], args[1]);
-    let argumentos = { primeiro: Number(args[0]), segundo: Number(args[1]) }
-    console.log(argumentos);
-    // const client = require('./client-grpc'); // Importando o modulo criado no passo anterior
-
-    client.calculandoMMC(argumentos, (error, response) => { // Chamando o metodo List RPC e retornando os dados no console
-        console.log('client calculando mmc');
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(response.resultado);
+    client.calculandoMMC(argumentos, (error, response) => { // Chamada do metodo RPC calculandoMMC
+        if (error) { // Se voltar um arro do callback
+            console.log(error); // Mostra na tela o erro
+        } else { // Senão tiver erro
+            console.log('O MMC é ' + response.resultado); // Printa o resultado do MMC
         }
     })
 }
 
-main();
-
-
+main(); // Executa a função main
